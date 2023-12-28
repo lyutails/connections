9ba0b25f3ff05726048b727503e25e07f030372b
lyutails <lyusya.cool@gmail.com>
2023-12-29 02:07:42 +0300

    feat: remove unnecessary console logs

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
 import { GroupsItems, UsersList } from '../../models/user.model';
+import { debounceTime, delay } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -26,25 +27,14 @@ export class MainComponent implements OnInit {
   }
 
   getGroups() {
-    const data = JSON.parse(localStorage.getItem('data')!);
-    const token = data['token'];
-    const uid = data['uid'];
-    const email = JSON.parse(localStorage.getItem('email')!);
-    console.log(uid, token, email);
-    this.userService.getGroups().subscribe((data) => {
-      console.log(data.Items);
+    this.userService.getGroups().pipe(delay(2000)).subscribe((data) => {
       const groupItems = data.Items;
       groupItems.map((group) => this.groups.push(group));
     });
   }
 
   getUsers() {
-    /* const data = JSON.parse(localStorage.getItem('data')!);
-    const token = data['token'];
-    const uid = data['uid'];
-    console.log(uid, token); */
-    this.userService.getUsers().subscribe((data) => {
-      console.log(data);
+    this.userService.getUsers().pipe(delay(2000)).subscribe((data) => {
       const users = data.Items;
       users.map((user) => this.users.push(user));
     });
